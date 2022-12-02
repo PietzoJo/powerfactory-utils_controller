b4da6af84c93a0ca47ea2ec627011ddde01d992f
DOM\s0458299 <johanna.pietzonka@posteo.de>
2022-12-02 16:43:57 +0100

    new folder controller with file controller.py to create controll instance of PowerFactory interface items

---
+++ powerfactory_utils/controller/controller.py
@@ -0,0 +1,7 @@
+@dataclass
+class PowerfactoryController:
+    project_name: str
+    grid_name: str
+    powerfactory_user_profile: str = ""
+    powerfactory_path: pathlib.Path = POWERFACTORY_PATH
+    powerfactory_version: str = powerfactory_version
